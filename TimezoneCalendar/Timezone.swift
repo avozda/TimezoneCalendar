b529dfb39479460ce7ed032e8c52b3ce1ad6eaeb
avozda <adamvozda88@seznam.cz>
2025-05-10 23:17:23 +0200

    add timezone model

---
+++ TimezoneCalendar/Timezone.swift
@@ -0,0 +1,51 @@
+//
+//  Timezone.swift
+//  TimezoneCalendar
+//
+//  Created by Adam Vožda on 10.05.2025.
+//
+import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+final class Timezone {
+    var name: String
+    var identifier: String
+    var colorHex: String?
+    @Relationship(deleteRule: .cascade, inverse: \Event.timezone)
+    var events: [Event] = []
+    
+    init(name: String, identifier: String, colorHex: String = "#007AFF") {
+        self.name = name
+        self.identifier = identifier
+        self.colorHex = colorHex
+    }
+    
+    var color: Color {
+        guard let hexColor = colorHex, !hexColor.isEmpty else {
+            return .blue
+        }
+        return Color(hex: hexColor) ?? .blue
+    }
+}
+
+// Color extension to support hex colors
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
+            return nil
+        }
+        
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+} 
